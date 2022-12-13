5bad86d0f3172714bc4ad58254653c7c54ae884f
seansg <seansgarwood@gmail.com>
2022-12-13 16:44:23 -0500

    Solve problem 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,6 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+  return string.repeat(num)
+}
 
 // Do not edit below this line
 module.exports = repeatString;
