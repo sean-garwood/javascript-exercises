8046a2e8bbb30f013e0391521e1851cdb353d310
seansg <seansgarwood@gmail.com>
2022-12-14 07:38:14 -0500

    Apply Gaussian method, pass test 1

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  if (num2 % 2 === 0) {
+    return ((num1 + num2) * num2 / 2);
+  } else {
+   return ((num1 + num2) * num2 / 2 + 1);
+  }
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+/* Gaussian method: check for evenness:
+
+if highest arg even:
+  sum args, multiply by highest arg, divide by two.
+if highest arg odd:
+  sum args, multiply by highest arg, divide by two, add one.
+*/
