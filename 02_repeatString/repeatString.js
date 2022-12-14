f4c24dac1e0302fa517b896e08169305d9326e92
seansg <seansgarwood@gmail.com>
2022-12-13 21:13:17 -0500

    Define reverseString for problem 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,7 +1,7 @@
 const repeatString = function(string, num) {
   if (num < 0) {
-    return 'ERROR'
-  } else return string.repeat(num) 
+    return 'ERROR';
+  } else return string.repeat(num);
 }
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,19 @@
-const reverseString = function() {
+/* need to:
+1. cast the string to an array
+2. use reverse() to reverse the elements
+3. rejoin everything into a string.
 
+reverse string function will take a string as an argument
+declare new variable, store array of string into it.
+reverse the array
+cast array to string
+return value
+*/
+
+const reverseString = function(string) {
+  let arrayOfString = string.split();
+  let reversed = arrayOfString.reverse();//reverse the array
+  return reversed.toString();
 };
 
 // Do not edit below this line
