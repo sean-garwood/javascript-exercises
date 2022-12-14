b91e95bece9d06f41389bddf99beea14e7564dc9
seansg <seansgarwood@gmail.com>
2022-12-14 16:26:17 -0500

    Handle larger first arg, edit algo for larger num being odd

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,16 @@
 const sumAll = function(num1, num2) {
-  if (num2 % 2 === 0) {
-    return ((num1 + num2) * num2 / 2);
+  if (num1 > num2) {
+    if (num1 % 2 === 0) {
+      return ((num2 + num1) * num1 / 2);
+    } else {
+      return ((num2 + num1) * Math.floor(num1 / 2) + (Math.ceil(num1 / 2)));
+    }
   } else {
-   return ((num1 + num2) * num2 / 2 + 1);
+    if (num2 % 2 === 0) {
+      return ((num1 + num2) * num2 / 2);
+    } else {
+      return ((num1 + num2) * Math.floor(num2 / 2) + (Math.ceil(num2 / 2)));
+    }
   }
 };
 
@@ -15,4 +23,9 @@ if highest arg even:
   sum args, multiply by highest arg, divide by two.
 if highest arg odd:
   sum args, multiply by highest arg, divide by two, add one.
+
+todo:
+
+-add error handling
+-add sort function that sorts two args from large to small (into an array?)
 */
