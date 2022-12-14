6a1882c8ac9a2a5bdc4605f89af0fb6698988da8
seansg <seansgarwood@gmail.com>
2022-12-13 22:21:43 -0500

    Pass 1st test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,28 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array = [], ...args) {
+  for (let i of arguments) {
+    const index = array.indexOf(i);
+    if (index > -1) {
+      array.splice(index, 1);
+    }
+  }
+  return array
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+/*
+i can edit wherever i please...
+
+take an array and return another array that has infinitely many of the original
+array's elements removed.
+
+the parameters of this function need to be an array with a default of empty, 
+and indices for the item(s) to remove.
+
+removeFromArray = function(array = [], ...args)
+for each i in args:
+find i in the array using indexOf()
+remove i from the array using splice()
+return modified array
+*/
