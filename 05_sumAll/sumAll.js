879e525be69545256986ffc483e3aff46e3cff1f
seansg <seansgarwood@gmail.com>
2022-12-14 21:13:58 -0500

    Catch cases where second arg is smaller than first

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,14 @@
 const sumAll = function(num1, num2) {
   let sum = 0;
-  for (num1; num1 <= num2; num1++) {//start at num1, go 
-    sum += num1;
-  } return sum;
+  if (num1 > num2) {
+    for (num2; num2 <= num1; num2++) {//start at num1, go 
+      sum += num2;
+    } return sum;
+  } else {
+    for (num1; num1 <= num2; num1++) {//start at num1, go 
+      sum += num1;
+    } return sum;
+  }
 };
 
 // Do not edit below this line
