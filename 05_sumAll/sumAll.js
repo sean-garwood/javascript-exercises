2cb87055c0e25f0c9932bf9acfa6822c0d9f0037
seansg <seansgarwood@gmail.com>
2022-12-14 21:20:03 -0500

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,14 +1,18 @@
 const sumAll = function(num1, num2) {
   let sum = 0;
-  if (num1 > num2) {
-    for (num2; num2 <= num1; num2++) {//start at num1, go 
-      sum += num2;
-    } return sum;
-  } else {
-    for (num1; num1 <= num2; num1++) {//start at num1, go 
-      sum += num1;
-    } return sum;
-  }
+  if (typeof num1 !== 'number' || typeof num2 !== 'number'
+      || num1 < 0 || num2 < 0) {
+        return 'ERROR' //not sure why we need to handle negatives
+      }
+    if (num1 > num2) {
+      for (num2; num2 <= num1; num2++) {
+        sum += num2;
+      } return sum;
+    } else {
+      for (num1; num1 <= num2; num1++) {
+        sum += num1;
+      } return sum;
+    }
 };
 
 // Do not edit below this line
