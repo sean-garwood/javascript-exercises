2803768edc4a8edf51cad33fdaecde0831a1cd77
seansg <seansgarwood@gmail.com>
2022-12-14 21:09:36 -0500

    Write for loop, pass trivial case

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,34 +1,9 @@
 const sumAll = function(num1, num2) {
-  if (num1 > num2) {
-    if (num1 % 2 === 0) {
-      return ((num2 + num1) * num1 / 2);
-    } else {
-      return ((num2 + num1) * Math.floor(num1 / 2) + (Math.ceil(num1 / 2)));
-    }
-  } else {
-    if (num2 % 2 === 0) {
-      return ((num1 + num2) * num2 / 2);
-    } else {
-      return ((num1 + num2) * Math.floor(num2 / 2) + (Math.ceil(num2 / 2)));
-    }
-  }
+  let sum = 0;
+  for (num1; num1 <= num2; num1++) {//start at num1, go 
+    sum += num1;
+  } return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
-
-/* Gaussian method: check for evenness:
-
-if highest arg even:
-  sum args, multiply by highest arg, divide by two.
-if highest arg odd:
-  sum args, multiply by highest arg, divide by two, add one.
-
-todo:
-
-it seems that this function only works when one of the args is equal to one.
-
-probably just need to do a for loop.
-
-
-*/
