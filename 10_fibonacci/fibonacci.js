8303c5a63ac237ab839be9fb8c645681335ea94a
seansg <seansgarwood@gmail.com>
2023-01-28 17:22:05 -0500

    First run

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+/*
 
+problem
+grab the nth+1 element of an array
+
+*/
+
+const fibonacci = (nthMember) => {
+  let fibArray = [1, 1];
+  for (let i = 1, i < nthMember; i++)
+    fibArray = fibArray.push(fibArray[i-1] + fibArray[i])
+  return fibArray[nthMember - 1];
 };
 
 // Do not edit below this line
