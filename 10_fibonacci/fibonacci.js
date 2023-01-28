fc8a11828475c9bc9422234f11c077278e9296fd
seansg <seansgarwood@gmail.com>
2023-01-28 17:36:29 -0500

    start 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -7,8 +7,10 @@ grab the nth+1 element of an array
 
 const fibonacci = (nthMember) => {
   let fibArray = [1, 1];
-  for (let i = 1, i < nthMember; i++)
-    fibArray = fibArray.push(fibArray[i-1] + fibArray[i])
+  for (let i = 1; i < nthMember; i++) {
+    fibArray.push(fibArray[i-1] + fibArray[i]);
+  }
+
   return fibArray[nthMember - 1];
 };
 
