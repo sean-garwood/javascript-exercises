ccf58a878c283aff7d0430fbd8450585291e5178
seansg <seansgarwood@gmail.com>
2023-01-29 13:14:48 -0500

    Solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
-};
-
-// Do not edit below this line
+const findTheOldest = function (people) {
+  for (let person of people) {
+    if (person.yearOfDeath === undefined) {
+      person.yearOfDeath = new Date().getFullYear();
+    } else continue;
+  }
+  people.sort((a, b) => {
+    if (a.yearOfDeath - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth) {
+      return 1;
+    } else return -1;
+  });
+  return people[0];
+}
 module.exports = findTheOldest;
