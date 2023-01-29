3ff4bb21fdfe19fdb837bbd1ac3449e1c087bf97
seansg <seansgarwood@gmail.com>
2023-01-28 21:46:33 -0500

    Solve 10"

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -6,6 +6,8 @@ grab the nth+1 element of an array
 */
 
 const fibonacci = (nthMember) => {
+  nthMember = Number(nthMember);
+  if (nthMember < 0) return 'OOPS'
   let fibArray = [1, 1];
   for (let i = 1; i < nthMember; i++) {
     fibArray.push(fibArray[i-1] + fibArray[i]);
